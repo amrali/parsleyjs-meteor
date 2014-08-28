da2793e71024e3c134d45a0a3834f4eee3bfb751
Amr Ali <amr.ali.cc@gmail.com>
2014-08-28 18:28:27 +0300

    Upgrade parsley.js to v2.0.5

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-  summary: "Javascript forms validation. Powerful, UX aware, and dead simple."
+  summary: "Javascript forms validation. Powerful, UX aware, and dead simple.",
+  version: "2.0.5",
+  name: "amr:parsley.js",
+  git: "https://github.com/amrali/parsleyjs-meteor.git"
 });
 
 Package.on_use(function (api) {
-  api.use('jquery', 'client');
-  api.use('less', 'client');
+  api.use('jquery@1.0.0', 'client');
+  api.use('less@1.0.0', 'client');
   api.add_files('parsley.min.js', 'client');
   api.add_files('parsley_variables.less', 'client');
   api.add_files('parsley.less', 'client');
